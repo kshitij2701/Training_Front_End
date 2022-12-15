42bb6f70664ac4900bcab0058ff78f612e837c91
kshitij2701 <kshitij.20b0101046@abes.ac.in>
2022-12-16 00:59:54 +0530

    login page & router done

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <style>
       <App />
-    </style>
   </React.StrictMode>
 );
 
